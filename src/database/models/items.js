4e03ce477724a2499d46ce257fea3dbffaa0f26e
Cedavilu Web Academy <113645784+cedavilu-web-academy@users.noreply.github.com>
2022-09-19 17:09:22 -0300

    Update items.js

--- src/database/models/items.js
+++ src/database/models/items.js
@@ -63,7 +63,7 @@ module.exports = function (sequelize, dataTypes) {
             foreignKey: "userId",
           });
         
-          Item.belongsTo(models.Dish, {
+          Item.belongsTo(models.Dishes, {
             as: "dish",
             foreignKey: "productId",
           });
