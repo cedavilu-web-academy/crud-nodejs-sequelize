aae8414ab92d76da710ef1d7d55f09663c40f03f
Cedavilu Web Academy <113645784+cedavilu-web-academy@users.noreply.github.com>
2022-09-19 15:56:06 -0300

    Update config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -15,10 +15,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "j0zkv9c04m8nle86ljsn",
+    "password": 'pscale_pw_qIJ8wgcQ8qN7NAAJF05lQ5pWpzG1gbi1Np6RH72Yh7y',
+    "database": "restaurante",
+    "host": "us-east.connect.psdb.cloud",
     "dialect": "mysql"
   }
 }
